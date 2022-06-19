b7f77295e13f90090cb6bca278bcefa440207849
RyanStore <107747308+RyanStore@users.noreply.github.com>
2022-06-19 19:25:03 +0800

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
-let teksbio = `*───────[ BIODATA OWNER ]───────*
+let teksbio = `*───────[ BIODATA OWNER #1 ]───────*
 *💌 Nama* : R
 *✉️ Nama RL* : Ryan Aditya Pratama
 *♂️ Gender* : Laki - laki
@@ -19,6 +19,50 @@ let teksbio = `*───────[ BIODATA OWNER ]───────*
 *🇫  Facebook* : https://www.facebook.com/profile.php?id=100078814156707
 *🏮 Chanel Youtube* : -
 *🐈 Github:* RyanStore
+
+#########
+
+*───────[ BIODATA OWNER #2 ]───────*
+*💌 Nama Panggilan* : Faisal
+*✉️ Nama Asli* : Rafi Lukmanu Faizal
+*♂️ Gender* : laki-laki
+*🕋 Agama* : islam
+*⏰ Tanggal lahir* : 05-06-2008
+*🎨 Umur* : 14
+
+*🧩 Hobby* :  Turu
+*💬 Sifat* : Idiot,Kanjut
+*🗺️ Tinggal* : Kediri Jawa timur
+
+*💔 Benci* : -
+
+*───────[ SOSIAL MEDIA ]───────*
+*📷 instagran* : @rafilukmanufaizal
+*🇫  Facebook* : Rafi Lukmanu Faisal
+*🏮 Chanel Youtube* : -
+*🐈 Github:* bntr
+
+#########
+
+*───────[ BIODATA OWNER #3 ]───────*
+*💌 Nama Panggilan* : *Cia*
+*✉️ Nama Asli* : *Citra oktaviani*
+*♂️ Gender* : *Perempuan*
+*🕋 Agama* : *Islam*
+*⏰ Tanggal lahir* : *10 Oktober 2007*
+*🎨 Umur* : *14*
+
+*🧩 Hobby* :  *Turu*
+*💬 Sifat* : Idiot,Kanjut
+*🗺️ Tinggal* : *Banyuwangi*
+
+*💔 Benci* : *Paisal*
+
+*───────[ SOSIAL MEDIA ]───────*
+*📷 instagran* : -
+*🇫  Facebook* : -
+*🏮 Chanel Youtube* : -
+*🐈 Github:* -
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
